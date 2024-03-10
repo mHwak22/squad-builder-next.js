11e138403bbe9bb39efcf45397698ab437ab0724
Rishav13773 <rishavjakhu>
2024-03-10 22:16:41 +0530

    build

--- app/api/getPlayerbyId/route.ts
+++ app/api/getPlayerbyId/route.ts
@@ -18,7 +18,7 @@ export async function GET(req: NextRequest): Promise<Response> {
     console.log(response.data.items);
 
     return new Response(JSON.stringify(response.data.items));
-  } catch (error) {
+  } catch (error: any) {
     console.error("Error fetching data:", error);
     return new Response(error);
   }
