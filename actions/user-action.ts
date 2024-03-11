15455f1cc23e81a27bb0d792cf1edeb40c6803fa
mHwak22 <mohakyo123@gmail.com>
2024-03-11 15:30:58 +0530

    change in newuser

--- actions/user-action.ts
+++ actions/user-action.ts
@@ -36,10 +36,12 @@ export async function createUser(emailAddress: string, firstName: string) {
     });
     console.log("New user created:", newUser);
 
+    if(newUser){
     cookies().set("user", JSON.stringify(newUser), { secure: true });
     // console.log("Cookie set for new user:", cookies().get("user"));
 
     return newUser;
+    }
   } catch (error) {
     console.error("Error occurred:", error);
   }
